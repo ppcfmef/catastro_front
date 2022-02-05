d706eb288d4a41b494b9aea2aab4cd0385f0078b
Omar Hernandez <ohernandez@gestyde.com>
2022-02-05 14:42:15 +0000

    fix: add module add edit in menu

--- src/app/modules/admin/security/modules/menu/containers/list/list.component.ts
+++ src/app/modules/admin/security/modules/menu/containers/list/list.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MatDrawer} from '@angular/material/sidenav';
 
 @Component({
     selector: 'app-list',
@@ -6,6 +8,11 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+
+    @ViewChild('matDrawer', {static: true}) matDrawer: MatDrawer;
+    drawerMode: 'side' | 'over';
+
+    title = 'Gestion de navegación';
 
     displayedColumns = ['nro', 'name', 'parent', 'status', 'actions'];
 
@@ -15,13 +22,36 @@ export class ListComponent implements OnInit {
     /**
      * Constructor
      */
-    constructor() {
+    constructor(
+        private _activatedRoute: ActivatedRoute,
+        private _changeDetectorRef: ChangeDetectorRef,
+        private _router: Router,
+    ) {
     }
 
     /**
      * On init
      */
     ngOnInit(): void {
+    }
+
+    /**
+     * On backdrop clicked
+     */
+    onBackdropClicked(): void {
+        // Go back to the list
+        this._router.navigate(['./'], {relativeTo: this._activatedRoute});
+
+        // Mark for check
+        this._changeDetectorRef.markForCheck();
+    }
+
+    redirectPage(id?: number): void {
+        // Go back to the list
+        this._router.navigate([id ? `./${id}` : './add'], {relativeTo: this._activatedRoute});
+
+        // Mark for check
+        this._changeDetectorRef.markForCheck();
     }
 
 }
