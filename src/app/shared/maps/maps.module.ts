7b04892e50959d4e02c8de80b1b5a80047b7aa89
Jose Ramirez <jcramireztello@gmail.com>
2022-07-04 02:19:06 -0500

    feat: integrate with land registry geolocation

--- src/app/shared/maps/maps.module.ts
+++ src/app/shared/maps/maps.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShowMapPointComponent } from './components/show-map-point/show-map-point.component';
-import { LandRegistryGeolocationComponent } from './components/land-registry-geolocation/land-registry-geolocation.component';
-
 
 
 @NgModule({
   declarations: [
     ShowMapPointComponent,
-    LandRegistryGeolocationComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ShowMapPointComponent,
-    LandRegistryGeolocationComponent,
   ]
 })
 export class MapsModule { }
