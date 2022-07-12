4e91ed956f61c38630842e914bdcd666d126d600
fsoto <franksoto2012@gmail.com>
2022-07-12 15:05:53 -0500

    fix del componente show-map-point

--- src/app/shared/maps/components/show-map-point/show-map-point.component.ts
+++ src/app/shared/maps/components/show-map-point/show-map-point.component.ts
@@ -1,21 +1,26 @@
 
 
 import { Coordinates } from '../../maps.type';
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild,OnChanges  } from '@angular/core';
 import { loadModules } from 'esri-loader';
 @Component({
   selector: 'app-show-map-point',
   templateUrl: './show-map-point.component.html',
   styleUrls: ['./show-map-point.component.scss']
 })
-export class ShowMapPointComponent implements OnInit,AfterViewInit {
+export class ShowMapPointComponent implements OnInit,AfterViewInit, OnChanges  {
     @Input() points: Coordinates[]=[{latitude: -13.53063, longitude: -71.955921},{latitude: -13.54, longitude: -71.955921}];
     @ViewChild('mapViewNode', { static: true }) private mapViewEl: ElementRef;
     title = 'Gestor Cartográfico';
     view: any = null;
     map: any;
 
-
+    simpleMarkerSymbol = {
+        type: 'picture-marker',  // autocasts as new PictureMarkerSymbol()
+        url: '/assets/images/map/location2.png',
+        width: '20px',
+        height: '30px'
+      };
   constructor() { }
 
   ngOnInit(): void {
@@ -24,13 +29,15 @@ export class ShowMapPointComponent implements OnInit,AfterViewInit {
 
   ngAfterViewInit(): void {
     //this.points=[{latitude: -13.53063, longitude: -71.955921}] ;
-    setTimeout(() => {this.initializeMap(this.points); }, 1000);
+    setTimeout(() => {this.initializeMap(); }, 1000);
     }
 
+  ngOnChanges(): void {
 
+    this.addPoints(this.points);
+    }
 
-
-  async initializeMap(inputPoints: Coordinates[]=[] ): Promise<void> {
+  async initializeMap( ): Promise<void> {
     try {
       const container = this.mapViewEl.nativeElement;
 
@@ -65,29 +72,14 @@ export class ShowMapPointComponent implements OnInit,AfterViewInit {
 
         map: this.map,
       };
+
       this.view = new MapView(mapViewProperties);
       const layer = new MapImageLayer({
         url: 'https://ws.mineco.gob.pe/serverdf/rest/services/Pruebas/CARTO_FISCAL_17/MapServer'
       });
       this.map.add(layer);
+      this.addPoints(this.points);
 /*
-      const simpleMarkerSymbol = {
-        type: 'simple-marker',
-        color: [3, 128, 255],  // Orange
-        outline: {
-            color: [255, 255, 255], // White
-            width: 1
-        }
-     };
-*/
-     const simpleMarkerSymbol = {
-        type: 'picture-marker',  // autocasts as new PictureMarkerSymbol()
-        url: '/assets/images/map/location2.png',
-        width: '20px',
-        height: '30px'
-      };
-
-
 if(inputPoints.length>0){
 
         const x=inputPoints[0].longitude;
@@ -102,17 +94,62 @@ if(inputPoints.length>0){
             };
             const pointGraphic = new Graphic({
                 geometry: point,
-                symbol: simpleMarkerSymbol
+                symbol: thosimpleMarkerSymbol
             });
             this.view.graphics.addMany([pointGraphic]);
         });
     }
-
+*/
 
     } catch (error) {
       console.error('EsriLoader: ', error);
     }
   }
 
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  async addPoints(inputPoints: Coordinates[]=[] ): Promise<void> {
+
+
+    const [
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Map,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        MapView,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Graphic,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        GraphicsLayer,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        MapImageLayer
+      ] = await loadModules([
+        'esri/Map',
+        'esri/views/MapView',
+        'esri/Graphic',
+        'esri/layers/GraphicsLayer',
+        'esri/layers/MapImageLayer'
+      ]);
+
+      this.view.graphics.removeAll();
+    if(inputPoints.length>0){
+
+        const x=inputPoints[0].longitude;
+        const y=inputPoints[0].latitude;
+        this.view.center = [x,y];
+        inputPoints.forEach((inputPoint: Coordinates)=>{
+
+            const point = { //Create a point
+                type: 'point',
+                longitude : inputPoint.longitude,
+                latitude: inputPoint.latitude
+            };
+            const pointGraphic = new Graphic({
+                geometry: point,
+                symbol: this.simpleMarkerSymbol
+            });
+            this.view.graphics.addMany([pointGraphic]);
+        });
+    }
+  }
 
 }
