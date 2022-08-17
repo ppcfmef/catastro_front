f324ebed6edf34438bb116e8cf433625d38eb806
Jose Ramirez <jcramireztello@gmail.com>
2022-08-17 09:07:26 -0500

    feat: create containers for Mapping.Metadata module

---
+++ src/app/modules/admin/mapping/metadata/components/metadata-manage-container/metadata-manage-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metadata-manage-container',
+  templateUrl: './metadata-manage-container.component.html',
+  styleUrls: ['./metadata-manage-container.component.scss']
+})
+export class MetadataManageContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/admin/mapping/metadata/metadata.module.ts
+++ src/app/modules/admin/mapping/metadata/metadata.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MetadataRoutingModule } from './metadata-routing.module';
 import { MetadataPage } from './pages/metadata/metadata.page';
 import { MetadataManagePage } from './pages/metadata-manage/metadata-manage.page';
+import { MetadataContainerComponent } from './components/metadata-container/metadata-container.component';
+import { MetadataManageContainerComponent } from './components/metadata-manage-container/metadata-manage-container.component';
 
 
 @NgModule({
   declarations: [
     MetadataPage,
-    MetadataManagePage
+    MetadataManagePage,
+    MetadataContainerComponent,
+    MetadataManageContainerComponent
   ],
   imports: [
     CommonModule,
