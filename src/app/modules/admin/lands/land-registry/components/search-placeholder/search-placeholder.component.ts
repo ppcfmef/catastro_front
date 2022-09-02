adade3ab146efaa3d5f5e59dec5ee8baf4ce237f
Ebed Guerra <ebedguerra@gmail.com>
2022-09-02 16:57:56 -0500

    feat: Agregado popu

--- src/app/modules/admin/lands/land-registry/components/search-placeholder/search-placeholder.component.ts
+++ src/app/modules/admin/lands/land-registry/components/search-placeholder/search-placeholder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-placeholder',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-placeholder.component.scss']
 })
 export class SearchPlaceholderComponent implements OnInit {
+
+  @Input() texto = 'texto prueba';
 
   constructor() { }
 
