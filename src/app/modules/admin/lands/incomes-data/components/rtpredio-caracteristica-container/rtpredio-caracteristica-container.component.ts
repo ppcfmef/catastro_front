9bc6a565e2deab9d5604d7b7c0f57b8d68f04d29
Jose ramirez <jcramireztello@gmail.com>
2023-09-08 10:14:11 -0500

    feat: add income data table for predio caracteristica

--- src/app/modules/admin/lands/incomes-data/components/rtpredio-caracteristica-container/rtpredio-caracteristica-container.component.ts
+++ src/app/modules/admin/lands/incomes-data/components/rtpredio-caracteristica-container/rtpredio-caracteristica-container.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { merge } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { IncomesDataService } from '../../services/incomes-data.service';
+import { startWith, switchMap } from 'rxjs/operators';
+import { IPagination } from 'app/core/common/interfaces/common.interface';
 
 @Component({
   selector: 'app-rtpredio-caracteristica-container',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RTPredioCaracteristicaContainerComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns = ['nro', 'ubigeo', 'anoAplicacion', 'anoConstruccion', 'clasificacionPred', 'materialPred', 'estadoConserva', 'totalAreaBienComun'];
+  dataSource = [];
+  count = 0;
+  pageIndex = 0;
+  pageSize = 5;
+
+  constructor(
+    private incomesDataService: IncomesDataService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    merge(this.paginator?.page)
+    .pipe(
+      startWith({}),
+      switchMap(() => {
+        const limit = this.paginator.pageSize;
+        const offset = limit * this.paginator.pageIndex;
+        const queryParams = { limit, offset };
+        return this.incomesDataService.getRTPredioCaracteristica(queryParams);
+      })
+    ).subscribe((response: IPagination<any>) => {
+        this.initialPaginator(response);
+    });
+  }
+
+  private initialPaginator(pagination: IPagination<any>): void {
+    this.count = pagination.count;
+    this.pageIndex = this.paginator.pageIndex;
+    this.pageSize = this.paginator.pageSize;
+    this.dataSource = pagination.results;
   }
 
 }
