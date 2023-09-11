5944d7117db29e3e26d260bd533f17afba94cc7e
Jose ramirez <jcramireztello@gmail.com>
2023-09-10 22:15:12 -0500

    feat: add income data table for Base imponible

--- src/app/modules/admin/lands/incomes-data/components/rtbase-imponible-container/rtbase-imponible-container.component.ts
+++ src/app/modules/admin/lands/incomes-data/components/rtbase-imponible-container/rtbase-imponible-container.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { merge } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { IncomesDataService } from '../../services/incomes-data.service';
+import { startWith, switchMap } from 'rxjs/operators';
+import { IPagination } from 'app/core/common/interfaces/common.interface';
 
 @Component({
   selector: 'app-rtbase-imponible-container',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RTBaseImponibleContainerComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns = ['nro', 'ubigeo', 'codContr', 'baseImponibleT', 'baseImponibleT1', 'fechaData'];
+  dataSource = [];
+  count = 0;
+  pageIndex = 0;
+  pageSize = 5;
+
+  constructor(
+    private incomesDataService: IncomesDataService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    merge(this.paginator?.page)
+    .pipe(
+      startWith({}),
+      switchMap(() => {
+        const limit = this.paginator.pageSize;
+        const offset = limit * this.paginator.pageIndex;
+        const queryParams = { limit, offset };
+        return this.incomesDataService.getRTBaseImponible(queryParams);
+      })
+    ).subscribe((response: IPagination<any>) => {
+        this.initialPaginator(response);
+    });
+  }
+
+  private initialPaginator(pagination: IPagination<any>): void {
+    this.count = pagination.count;
+    this.pageIndex = this.paginator.pageIndex;
+    this.pageSize = this.paginator.pageSize;
+    this.dataSource = pagination.results;
   }
 
 }
