30aadf7b6095dcad9179f2d834ea4505b7dd9cc3
Gabriela Siclla <92539220+GabNatali@users.noreply.github.com>
2024-04-28 21:30:37 -0500

    style:submodulo acumulación

--- src/app/modules/admin/lands/maintenance/components/maintenance-accumulation-container/maintenance-accumulation-container.component.ts
+++ src/app/modules/admin/lands/maintenance/components/maintenance-accumulation-container/maintenance-accumulation-container.component.ts
@@ -39,9 +39,9 @@ export class MaintenanceAccumulationContainerComponent implements OnInit,OnChang
     disabled =false;
     _unsubscribeAll: Subject<any> = new Subject<any>();
     data = {
-      title: "Volver a la Lista de Predios",
-      routerLink: "/land/maintenance/list",
-    }
+      title: 'Volver a la Lista de Predios',
+      routerLink: '/land/maintenance/list',
+    };
   constructor(
     private landMaintenanceService: LandMaintenanceService,
     private _userService: UserService,
