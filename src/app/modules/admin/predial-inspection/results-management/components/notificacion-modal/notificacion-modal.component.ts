505da6fd6a858eb7444d6692dfad1ba1639af5d9
FRANK SOTO PEÑA <franksotogithub>
2024-09-13 09:10:22 -0500

    actualizando modal

--- src/app/modules/admin/predial-inspection/results-management/components/notificacion-modal/notificacion-modal.component.ts
+++ src/app/modules/admin/predial-inspection/results-management/components/notificacion-modal/notificacion-modal.component.ts
@@ -6,6 +6,15 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./notificacion-modal.component.scss']
 })
 export class NotificacionModalComponent implements OnInit {
+  texto1: string;
+  texto2: string;
+
+  texto =`Tenga  en  cuenta  que  el  incumplimiento  de  presentar  las  declaraciones  que  contengan  la  determinación  de  la  deuda  tributaria  dentro  de  los
+  plazos  establecidos,  así  como  presentar  otras  declaraciones  o  comunicaciones  de  manera  incompleta  o  no  conformes  con  la  realidad,
+  constituyen faltas según lo establecido en el Artículo 176, Numerales 1 y 4 del Código Tributario. Las sanciones por estas faltas pueden variar
+  desde  el  30%  hasta  el  50%  de  la  UIT.  Para  lo  cual  tiene  5  días  hábiles  para  apersonase  a`;
+
+  textoFinal ='bajo apercibimiento de iniciar el procedimiento de fiscalización y sanción.';
 
   constructor(
     public dialogRef: MatDialogRef<NotificacionModalComponent>,
@@ -19,6 +28,7 @@ export class NotificacionModalComponent implements OnInit {
 
 
       save(): void{
+        this.dataDialog.textoEditar = `${this.texto} ${this.texto1} o  comunicarse a ${this.texto2} ${this.textoFinal}`;
         this.dialogRef.close( this.dataDialog.textoEditar);
       }
         ngOnInit(): void {
